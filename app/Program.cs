85b90de5688a643f811b4382357a85afc49dfc5e
maddenvvs <maddenvvs@gmail.com>
2020-07-09 15:49:43 +0400

    Add csharp starter

---
+++ app/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace app
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var serverUrl = new Uri(args[0], UriKind.Absolute);
+            var playerKey = args[1];
+
+            Console.WriteLine($"ServerUrl: {serverUrl}; PlayerKey: {playerKey}");
+
+            using var httpClient = new HttpClient {BaseAddress = serverUrl};
+            using var responseMessage = await httpClient.GetAsync($"?playerKey={playerKey}");
+            responseMessage.EnsureSuccessStatusCode();
+        }
+    }
+}
