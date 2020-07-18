a8af140b0460c57f5a2a54129044a7c003c1c2fb
MacBook <macbook@Savchenkos-MacBook-Pro.local>
2020-07-18 14:43:55 +0400

    added negate func

--- app/lib/icfpc/functions.rb
+++ app/lib/icfpc/functions.rb
@@ -2,11 +2,15 @@ require 'set'
 
 module ICFPC
 	module Functions
-		FUNCTION_NAMES = Set["add"]
+		FUNCTION_NAMES = Set["add", "negate"]
 
 		class << self
 			def add x1, x2
 				x1 + x2
+			end
+
+			def negate x1
+				-x1
 			end
 		end
 	end
