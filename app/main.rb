124edde680ce8abb971fa23159720c0ce0894f2b
sofinms <sofinms@gmail.com>
2020-07-19 14:49:27 +0400

    try multi join sender

--- app/main.rb
+++ app/main.rb
@@ -3,25 +3,49 @@ $:.unshift File.join(__dir__, 'lib')
 require 'net/http'
 require 'icfpc/functions'
 
+def send serverurl, post_paramenters
+  uri = URI(serverurl)
+
+  modulated_params = ICFPC::Functions.mod(post_paramenters)
+  res = Net::HTTP.post(uri, modulated_params)
+  if res.code == "200"
+    demodulated_response = ICFPC::Functions.dem(res.body)
+    puts "Server response: %s" % demodulated_response.to_s
+  else
+    puts "Unexpected server response:"
+    puts "HTTP code: %s" % res.code
+    puts "Response body: %s" % res.body
+    exit(2)
+  end
+end
+
 def main
   begin
     serverurl = ARGV[0]
     playerkey = ARGV[1]
-    puts "ServerUrl: %s; PlayerKey: %s" % [serverurl, playerkey]
+    post_paramenters = [2, playerkey.to_i, nil]
+    puts "ServerUrl: %s; PlayerKey: %s; post_paramenters: %s" % [serverurl, playerkey, post_paramenters]
+    send serverurl, post_paramenters
 
-    uri = URI(serverurl)
-    post_paramenters = [2, 232432, 34,243]
-    modulated_params = ICFPC::Functions.mod(post_paramenters)
-    res = Net::HTTP.post(uri, modulated_params)
-    if res.code == "200"
-      puts "Server response: %s" % res.body
-    else
-      puts "Unexpected server response:"
-      puts "HTTP code: %s" % res.code
-      puts "Response body: %s" % res.body
-      exit(2)
-    end
+    post_paramenters = [2, playerkey.to_i, [1]]
+    puts "ServerUrl: %s; PlayerKey: %s; post_paramenters: %s" % [serverurl, playerkey, post_paramenters]
+    send serverurl, post_paramenters
 
+    post_paramenters = [2, playerkey.to_i, [2]]
+    puts "ServerUrl: %s; PlayerKey: %s; post_paramenters: %s" % [serverurl, playerkey, post_paramenters]
+    send serverurl, post_paramenters
+
+    post_paramenters = [2, playerkey.to_i, [3]]
+    puts "ServerUrl: %s; PlayerKey: %s; post_paramenters: %s" % [serverurl, playerkey, post_paramenters]
+    send serverurl, post_paramenters
+
+    post_paramenters = [2, playerkey.to_i, [4]]
+    puts "ServerUrl: %s; PlayerKey: %s; post_paramenters: %s" % [serverurl, playerkey, post_paramenters]
+    send serverurl, post_paramenters
+
+    post_paramenters = [2, playerkey.to_i, [5]]
+    puts "ServerUrl: %s; PlayerKey: %s; post_paramenters: %s" % [serverurl, playerkey, post_paramenters]
+    send serverurl, post_paramenters
   rescue StandardError => e
     puts "Unexpected server response:"
     puts e
