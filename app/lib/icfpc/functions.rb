29a3545a3f6e56660c1754a4d2b9ecdbbae4b2bf
Alexandr Fedorov <alexandrfedorov@gmail.com>
2020-07-19 12:16:59 +0400

    reworked demodulate + fixed bugs

--- app/lib/icfpc/functions.rb
+++ app/lib/icfpc/functions.rb
@@ -46,12 +46,11 @@ module ICFPC
 
 			def dem num_as_bit_string
 				return 0 if num_as_bit_string == '010'
-				num_chars = num_as_bit_string.chars
-				signal = num_chars.shift(2).join
-				matched_string = num_chars.join.match(/1+0+(1[0-1]+)/)
-				num_abs = matched_string[1].to_i(2)
+				m = num_as_bit_string.match(/^(10|01)1+0+([01]+)$/)
+				signal = m[1]
+				num_abs = m[2].to_i(2)
 				if signal == '10'
-					num_abs = 0 - num_abs
+					num_abs = (-num_abs)
 				end
 				num_abs
 			end
