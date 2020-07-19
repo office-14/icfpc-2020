41a52cf89b79300a49b106b50a7b8952d1d445dd
sofinms <sofinms@gmail.com>
2020-07-19 14:02:42 +0400

    add join request

--- app/main.rb
+++ app/main.rb
@@ -1,4 +1,7 @@
+$:.unshift File.join(__dir__, 'lib')
+
 require 'net/http'
+require 'icfpc/functions'
 
 def main
   begin
@@ -7,7 +10,9 @@ def main
     puts "ServerUrl: %s; PlayerKey: %s" % [serverurl, playerkey]
 
     uri = URI(serverurl)
-    res = Net::HTTP.post(uri, playerkey)
+    post_paramenters = [2, playerkey.to_i, [nil]]
+    modulated_params = ICFPC::Functions.mod(post_paramenters)
+    res = Net::HTTP.post(uri, modulated_params)
     if res.code == "200"
       puts "Server response: %s" % res.body
     else
@@ -16,6 +21,7 @@ def main
       puts "Response body: %s" % res.body
       exit(2)
     end
+
   rescue StandardError => e
     puts "Unexpected server response:"
     puts e
