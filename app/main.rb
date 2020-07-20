86f96c258db9ee01f4bf83b6329ba08d80cc7c70
sofinms <sofinms@gmail.com>
2020-07-20 16:07:37 +0400

    XXX 2

--- app/main.rb
+++ app/main.rb
@@ -77,7 +77,7 @@ def main
           begin
             shoot_x = answer_action[3][2][0][0][2][0]
             shoot_y = answer_action[3][2][0][0][2][1]
-            commands.push [2, ship, ICFPC::Cons.new([shoot_x,shoot_y]), 1]
+            commands.push [2, ship, ICFPC::Cons.new([shoot_x-2,shoot_y+2]), 1]
           rescue
             commands.push [2, ship, ICFPC::Cons.new(variables_move.sample(1)[0])]
           end
